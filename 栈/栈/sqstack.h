99138fea87b1868b1ae89ddc16e271aeb3067cd1
Caoyuewin <849807784@qq.com>
2019-03-27 22:49:15 +0800

    2019.03.27

--- 栈/栈/sqstack.h
+++ 栈/栈/sqstack.h
@@ -12,7 +12,23 @@ typedef struct {
 	int stacksize;
 }Stack;
 
-void Initstack(Stack s) {
+void Init(Stack* s) {
+	s->stacksize = 10;
+	s->base = malloc(sizeof(ElemType)*s->stacksize);
+	s->top = s->base;
 
+}
 
+void Push(Stack* s, ElemType val) {
+	if ((s->top - s->base) == s->stacksize)
+		return;
+	*s->top++ = val;
+}
+
+void Pop(Stack* s,ElemType val) {
+	if (s->base == s->top)
+	{
+		return;
+	}
+	val = *--s->top;
 }
