1c2b55e8ffd94cddc7f8331851680e10dd7016a7
Caoyuewin <849807784@qq.com>
2019-04-04 08:54:56 -0400

    2019.04.04

--- 二叉树/非递归创建二叉树/Create.cpp
+++ 二叉树/非递归创建二叉树/Create.cpp
@@ -1,2 +1,19 @@
 #include <stack>
 
+
+void PreorderNoR(Node* root){
+  stack<Node *> s;
+  Node *cur = root;
+
+  while(cur != (!s.empty() || cur != NULL){
+      while(cur != NULL) {
+      printf("%c", cur->value);
+      s.push(cur);
+      cur = cur->left;
+      }
+
+      Node* top
+  }
+
+
+}
