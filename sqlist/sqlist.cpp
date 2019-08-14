7669edf411623cb3ed3355dd285d2495b8d3107f
Caoyuewin <849807784@qq.com>
2019-08-14 04:57:29 -0400

    SeqList

--- sqlist/sqlist.cpp
+++ sqlist/sqlist.cpp
@@ -1,92 +1,105 @@
 #include <iostream>
-#include <malloc.h>
+#include <algorithm>
 
 using namespace std;
 
-#define MAX 3
-typedef int Datatype;
-
-class sqlist{
-  public:
-//构造函数
-sqlist(int size, int *array){
-  _capacity = MAX;
-  _array = new Datatype[MAX];
-  _size = size; 
-  int i = 0;
-  for(; i < size; i++){
-    _array[i] = array[i];
-  }
-}
-//析构函数
-~sqlist(){
-  _capacity = 0;
-  delete[] _array;
-  _size = 0;
-}
-    void HeadInsert(Datatype data);
-    void TailInsert(Datatype data);
-    void HeadErase();
-    void TailErase();
-    void Insert(Datatype data);
-    void Erase(int pos);
-    void IncrtCap();
-    void PrintList();
-  private:
-    int _size;
-    Datatype* _array;
-    int _capacity;
+template <class T>
+class SeqList{
+    public:
+        SeqList(size_t size)
+            : _size(0), _capacity(size)
+        {
+            _array = new T[size];
+        }
+        ~SeqList() {
+            delete[] _array;
+        }
+        //头插
+        void HeadInsert(T data) {
+            CheckCapacity();
+            for(int i = _size; i > 0; --i) {
+                _array[i+1] = _array[i];
+            } 
+            _array[0] = data;
+            ++_size;
+        }
+        //头删
+        void HeadErase() {
+            for(int i = 0; i < _size; ++i) {
+                _array[i] = _array[i+1];
+            }
+            --_size;
+        } 
+        //中间插入
+        void Insert(T data, int pos) {
+            CheckCapacity();
+            for(int i = _size; i > pos; --i) {
+                _array[i+1] = _array[i];
+            }
+            _array[pos] = data;
+            ++_size;
+        }
+        //中间删除
+        void Erase(int pos) {
+            for(int i = pos; i < pos; ++i) {
+                _array[i] = _array[i+1];
+            }
+            --_size;
+        }
+        //尾插
+        void TailInsert(T data) {
+            CheckCapacity();
+            _array[_size+1] = data;
+            ++_size;
+        }
+        //尾删
+        void TailErase() {
+            --_size;
+        }
+        //扩容函数
+        void CheckCapacity(){
+            if(_capacity == _size) {
+                //扩容为两倍
+                _capacity *= 2;
+                //开辟空间
+                T* tmp = new T[_capacity];
+                //搬移元素
+                for(int i = 0; i < _size; ++i) {
+                    tmp[i] = _array[i];
+                }
+                swap(tmp, _array);
+                //释放资源
+                delete[] tmp;
+            }
+        }
+        //打印函数
+        void Print() {
+            for(int i = 0; i < _size; ++i) {
+                cout << _array[i] << " ";
+            }
+            cout << endl;
+        }
+    private:
+        int _size;
+        int _capacity;
+        T* _array;
 };
 
-void sqlist::HeadInsert(Datatype data){
-  if(_size == _capacity)
-    IncrtCap();
-  int i = _size;
-  for(; i > 0; i--){
-    _array[i] = _array[i-1];
-    
-  }
-  _array[0] = data; 
-  _size++;
+
+void TestFunc() {
+    SeqList<int> list(3);
+    list.HeadInsert(5);
+    list.HeadInsert(4);
+    //list.HeadInsert(3);
+    //list.Insert(10, 2);
+    //list.TailInsert(6);
+    list.Print();
 }
 
-void sqlist::TailInsert(Datatype data){
-  if(_size == _capacity)
-    IncrtCap();
-  _array[_size] = data;
-  _size++;
-}
 
-void sqlist::IncrtCap(){
-  _array = new Datatype[2*_capacity];
-  _capacity *= 2;
-}
-
-void sqlist::HeadErase(){
-  int i = 0;
-  for(; i < _size -2; i++){
-    _array[i] = _array[i+1];
-  }
-  _size--;
-}
-
-void sqlist::TailErase(){
-  _size--;
-}
-    
-void sqlist::PrintList(){
-  int i = 0;
-  for(; i < _size; i++){
-    cout<<_array[i]<<" ";
-  }
-  cout<<endl;
-}
-    
 int main() {
-  int a[2] = {1,2};
-  sqlist list(sizeof(a) / sizeof(a[0]), a);
-  list.HeadInsert(0);
-  list.TailInsert(3);
-  list.PrintList();
-  return 0;
+    TestFunc();
+
+    return 0;
 }
+
